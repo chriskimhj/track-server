01a43540d294ff4e37c668e7f86a4db4adfa23d9
Chris Kim <hjchristopher.dev@gmail.com>
2023-07-27 20:36:14 -0700

    Implement JWT and password hashing

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
+const requireAuth = require('./middlewares/requireAuth');
 require('dotenv').config();
 
 const app = express();
@@ -18,8 +19,8 @@ mongoose.connection.on('error', (err) => {
     console.error('Error connecting to mongo', err);
 });
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.get('/', requireAuth, (req, res) => {
+    res.send(`Your Email: ${req.user.email}`);
 });
 
 app.listen(3000, () => {
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 const User = mongoose.model('User');
+require('dotenv').config();
 
 const router = express.Router();
 
@@ -10,11 +12,33 @@ router.post('/signup', async (req, res) => {
         const user = new User({email, password});
         await user.save();
     
-        res.send('You made a post request');
+        const token = jwt.sign({ userId: user._id }, process.env.SECRETKEY);
+        res.send({ token });
     } catch (err) {
         return res.status(422).send(err.message);
     }
 
+});
+
+router.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
+
+    if(!email || !password) {
+        return res.status(422).send({ error: 'Must provide email and password.'});
+    }
+
+    const user = await User.findOne({ email });
+    if(!user) {
+        return res.status(422).send({ error: 'Invalid password or email.'});
+    }
+
+    try {    
+        await user.comparePassword(password);
+        const token = jwt.sign({ userId: user._id }, process.env.SECRETKEY);
+        res.send({ token });
+    } catch(err) {
+        return res.status(422).send({ error: 'Invalid password or email.'});
+    }
 });
 
 module.exports = router;
